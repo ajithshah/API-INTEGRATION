aef0bd1f2364633b9df5d9c146663c29cc7ef194
ajith shah <ajithshah37@gmail.com>
2022-09-03 15:13:18 +0530

    [Ajith] Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Login from './components/Login';
+import Profile from './components/Profile';
 
 function App() {
+  const [userList]= useState([])
   return(
     <div className='app'>
-      <Login />
+      {userList ? <Profile/> : <Login />}
     </div>
   )
 }
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -4,6 +4,7 @@ function Profile() {
   return (
     <div>
       <h1>Profile</h1>
+      <p>Username: {  }</p>
     </div>
   )
 }
